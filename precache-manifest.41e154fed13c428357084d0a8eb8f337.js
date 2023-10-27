cbb487fb52702e6a3b452f475db9d64933b8c2bc
Jacob King <jacobuid@gmail.com>
2023-10-26 19:16:26 -0500

    Updates

---
+++ precache-manifest.41e154fed13c428357084d0a8eb8f337.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e718a674f8364e4ceb69f1226f6a895",
+    "url": "/mm-character-builder/index.html"
+  },
+  {
+    "revision": "7c4d867fa5651c6d7dd8",
+    "url": "/mm-character-builder/static/css/main.c08742c4.chunk.css"
+  },
+  {
+    "revision": "855a1c7f9cac89c87363",
+    "url": "/mm-character-builder/static/js/2.9bf38e5c.chunk.js"
+  },
+  {
+    "revision": "7c4d867fa5651c6d7dd8",
+    "url": "/mm-character-builder/static/js/main.0996dba8.chunk.js"
+  },
+  {
+    "revision": "35b655968277648ccce3",
+    "url": "/mm-character-builder/static/js/runtime-main.c2e7632e.js"
+  }
+]);
